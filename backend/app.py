c58011532eb6db5ac1d210933d7f7d206ea887f5
KennBro <kennbro@protonmail.com>
2020-10-29 23:53:09 -0300

    [ADD] Multiprocessing

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 # -*- encoding: utf-8 -*-
 import argparse
 
-from flask import Flask, Blueprint, request # , jsonify
+from flask import Flask, Blueprint, request  # , jsonify
 from flask_cors import CORS
+
+import multiprocessing
+import http.server
+import socketserver
 
 import modules.instagram.instagram_avatar as instagram
 import modules.fecaldetect.fecaldetect as fecaldetect
@@ -65,15 +69,49 @@ def r_recog():
     return result
 
 
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory="../frontend/dis/", **kwargs)
+
+
+def startHttpServer():
+    PORT = 9001
+    # TODO : Add directory validation
+    with socketserver.TCPServer(("", PORT), Handler) as httpd_server:
+        print("serving at port", PORT)
+        httpd_server.serve_forever()
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--ip', action='store', default='127.0.0.1',
                         help='IP address')
+    parser.add_argument('-e', '--env', action='store', default='dev',
+                        help='Environment [dev, prod]')
 
     args = parser.parse_args()
     ip = str(args.ip)
+    env = str(args.env)
 
     CORS(app)
     home = Blueprint('home_views', __name__)
     app.register_blueprint(home)
-    app.run(host=ip, port=9002, debug=True)
+
+    if (env == "prod"):
+
+        kwargs_flask = {"host": ip, "port": 9002}
+        flask_proc = multiprocessing.Process(name='flask',
+                                             target=app.run,
+                                             kwargs=kwargs_flask)
+        flask_proc.daemon = True
+
+        httpd_proc = multiprocessing.Process(name='httpd',
+            target=startHttpServer)
+        httpd_proc.daemon = True
+
+        httpd_proc.start()
+        flask_proc.start()
+        flask_proc.join()
+        httpd_proc.join()
+    else:
+        app.run(host=ip, port=9002, debug=True)
